e5b2b7e199dcc96d526a6eb5a4f7c426dc338ef1
pagh2322 <pagh2322@naver.com>
2022-07-22 01:20:07 +0900

    initialize project

---
+++ Bitcoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bitcoin
+//
+//  Created by peo on 2022/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
